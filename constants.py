ef1b6b41520302b7fa4dcbabf4d431e00e83befc
ImpendingKataclysm <ktjanzen42@gmail.com>
2024-03-27 14:37:50 -0700

    Load a basic built in tile map with a 'Platforms' layer and a 'Coins' layer when the player starts the game.

--- constants.py
+++ constants.py
@@ -6,3 +6,14 @@ SCREEN_TITLE = 'Simple Platformer'
 # Menu Display
 MAIN_MENU_TEXT = 'Main Menu - Click to play'
 MENU_FONT_SIZE = 30
+
+# Game Map
+MAP_PATH = ':resources:tiled_maps/'
+BASIC_MAP = f'{MAP_PATH}map.json'
+
+# Map Layers
+COINS_LAYER = 'Coins'
+PLATFORMS_LAYER = 'Platforms'
+
+# Sprite Scaling
+TILE_SCALING = 0.5
--- game_view.py
+++ game_view.py
@@ -1,4 +1,6 @@
 import arcade
+import os
+import constants as c
 
 
 class GameView(arcade.View):
@@ -8,15 +10,52 @@ class GameView(arcade.View):
     def __init__(self):
         super(GameView, self).__init__()
 
+        # Set the file path to start with this program.
+        file_path = os.path.dirname(os.path.abspath(__file__))
+        os.chdir(file_path)
+
+        # Set up the map and scene
+        self.tile_map = None
+        self.scene = None
+
+    def setup(self):
+        """
+        Loads the scene and map and sets the game state.
+        """
+        # Create the game map
+        map_name = c.BASIC_MAP
+        layer_options = {
+            c.COINS_LAYER: {
+                'use_spatial_hash': True,
+            },
+            c.PLATFORMS_LAYER: {
+                'use_spatial_hash': True
+            },
+        }
+
+        self.tile_map = arcade.load_tilemap(
+            map_name,
+            c.TILE_SCALING,
+            layer_options
+        )
+
+        # Initialize the Scene from the map
+        self.scene = arcade.Scene.from_tilemap(self.tile_map)
+
+        # Set the background color based on the map
+        if self.tile_map.background_color:
+            arcade.set_background_color(self.tile_map.background_color)
+
     def on_show_view(self):
         """
         Display the game in its initial state.
         """
-        arcade.set_background_color(arcade.color.AIR_SUPERIORITY_BLUE)
+        self.setup()
 
     def on_draw(self):
         """
         Render the game map and sprites.
         """
         self.clear()
+        self.scene.draw()
     
